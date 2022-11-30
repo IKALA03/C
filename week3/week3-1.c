cecf8a010f74634bcc807d0b0d294c851c5fb0d3
IKALA03 <108133197+IKALA03@users.noreply.github.com>
2022-11-30 12:53:03 +0100

    Update week3-1.c

--- week3/week3-1.c
+++ week3/week3-1.c
@@ -1,36 +1,46 @@
 #include <stdio.h>  
-float *maxArrays(float *a, float *b, float *arr3)  
+
+float * maxArrays(float *array1, float *array2, float *array3)  
 {
 
     for(int i=0;i<4;i++)  
     {  
-        if (a[i] <= b[i]) {
-            arr3[i] = b[i];
+        if (array1[i] <= array2[i]) {
+            array3[i] = array1[i];
         }
         else {
-            arr3[i] = a[i];
+            array3[i] = array2[i];
         }
     }
 
-    return arr3;  
+    return array3;  
 }  
 
 int main()  
 {  
     float arr1[] = {0.7, 3.3, 0.5, 10.3};
-
     float arr2[] = {4.1, 1.5, 0.5, 2.3};
-
     float arr3[4];
-
-    float *n;  
+    float * result;  
     
-    n=maxArrays(arr1, arr2, arr3);  
+    printf("Array 1: ");
+    for (int i = 0; i <= 3; i++) {
+        printf("%f ", arr1[i]);
+    }
+    printf("\nArray 2: ");
+    for (int i = 0; i <= 3; i++) {
+        printf("%f ", arr2[i]);
+    }    
+    printf("\n");
+
+
+    result = maxArrays(arr1, arr2, arr3);  
 
     printf("\nElements of array are :");  
-    for(int i=0;i<4;i++)  
-        {  
-            printf("%lf ", n[i]);  
-        }  
-        return 0;  
+    for (int i=0;i<4;i++)  
+    {  
+        printf("%lf ", result[i]);  
+    }
+
+    return 0;  
 }  
