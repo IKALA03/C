a2d714108be6ab35cae10aa0724e5155ce7b485c
IKALA03 <108133197+IKALA03@users.noreply.github.com>
2022-12-02 19:41:20 +0100

    Update week3-3.c

--- week3/week3-3.c
+++ week3/week3-3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 2
+#define MAX 3
 
 struct opleiding {
     char opleidingNaam[30];
@@ -12,27 +12,38 @@ struct student {
     char naam[15];
     int leeftijd;
     struct opleiding opleiding;
-} data[MAX] = { {"ik", 19, "ict", 2},
-    {"jij", 18, "ict", 2}
-};
+} data[MAX];
 
 struct student *s;
 int count;
+int i;
 
 int main (void) {
+    printf("3 namen (gesplits door een spatie): \n");
+    scanf("%s %s %s", data[0].naam, data[1].naam, data[2].naam); // vraag om naam
+
+    for (i=0; i<MAX; i++) {  // for loop om informatie op te vragen (totaal 3 keer)
+        printf("\nleeftijd van %s: ", data[i].naam);
+        scanf("%d", &data[i].leeftijd); // vraag om leeftijd
+
+        printf("\nOpleiding van %s: ", data[i].naam);
+        scanf("%s", data[i].opleiding.opleidingNaam); // vraag om opleiding naam
+
+        printf("\nInstroomjaar van %s op %s: ", data[i].naam, data[i].opleiding.opleidingNaam);
+        scanf("%d", &data[i].opleiding.instroomjaar); // vraag om instroomjaar van de opleiding
+    }
+
     s = data;
 
-    for (count = 0; count < MAX; count++)
+    for (count = 0; count < MAX; count++) 
     {
-                    /* Illustration that incrementing a pointer increases it by */
-                    /* an amount equal to the size of the data object it points to */
-    printf("At address %d: %s %d %s %d\n", 
-            (int) s, 
+        printf("Element %d: Naam: %s, leeftijd: %d, opleiding: %s, instroomjaar: %d\n", // print elk element
+            count, 
             s->naam,
             s->leeftijd,
             s->opleiding.opleidingNaam,
             s->opleiding.instroomjaar);
-    s++;   
+            s++;   
     }
     
     return 0;
