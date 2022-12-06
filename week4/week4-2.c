68f6f135cad5c64b23a2d5de3ad195aacd293626
IKALA03 <108133197+IKALA03@users.noreply.github.com>
2022-12-06 10:17:36 +0100

    Update week4-2.c

--- week4/week4-2.c
+++ week4/week4-2.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct persoon {
-    char[15] naam;
-    int leeftijd;
-}
+  char naam[15];
+  int leeftijd;
+};
 
-int main( void ) {
-    return 0;
+int main() {
+  struct persoon *pointer;
+  int records;
+  
+  printf("Vul het aantal personen in dat je wilt toevoegen: ");
+  scanf("%d", &records);
+
+  pointer = (struct persoon *)malloc(records * sizeof(struct persoon));
+  for (int i = 0; i < records; ++i) {
+    printf("Vul de naam en leeftijd in:\n");
+    scanf("%s %d", (pointer + i)->naam, &(pointer + i)->leeftijd);
+  }
+
+  printf("Ingevulde informatie:\n");
+  for (int i = 0; i < records; ++i) {
+    printf("Naam: %s\tLeeftijd: %d\n", (pointer + i)->naam, (pointer + i)->leeftijd);
+  }
+
+  free(pointer);
+
+  return 0;
 }
