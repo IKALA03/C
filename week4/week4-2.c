3012a17972a73501145632d1f21a5587086bcd1a
IKALA03 <108133197+IKALA03@users.noreply.github.com>
2023-01-10 19:24:38 +0100

    Update week4-2.c

--- week4/week4-2.c
+++ week4/week4-2.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct persoon {
-    char naam[15];
-    int leeftijd;
-}; // structure peroon, bevat naam en leeftijd
 
 int main() {
-    struct persoon *pointer;
-    int records; // aantal records
-    
-    printf("Vul het aantal personen in dat je wilt toevoegen: ");
-    scanf("%d", &records); // vraag de gebruiker om hoeveel personen er toegevoegd moeten worden
+    struct persoon {
+    char naam[15];
+    int leeftijd;
+  };
 
-    pointer = (struct persoon *)malloc(records * sizeof(struct persoon)); // reserveer geheugen
-    for (int i = 0; i < records; ++i) { // voor ieder persoon wordt naam en leeftijd gevraagd
-        printf("Vul de naam en leeftijd in:\n"); 
-        scanf("%s %d", (pointer + i)->naam, &(pointer + i)->leeftijd);
-    }
-
-    printf("Ingevulde informatie:\n");  // laat de data zien die de gebruiker toegevoegd heeft
-    for (int i = 0; i < records; ++i) {
-        printf("Naam: %s\tLeeftijd: %d\n", (pointer + i)->naam, (pointer + i)->leeftijd);
-    }
+  struct persoon *pointer;
   
-    free(pointer); // maak geheugen vrij
+  pointer = (struct persoon *)malloc(sizeof(struct persoon));
 
-    return 0;
+
+  int i = 0;
+  while(1) {
+    printf("Vul de naam en leeftijd in:\n");
+    scanf("%s %d", (pointer + i)->naam, &(pointer + i)->leeftijd);
+
+    i++;
+
+    printf("Ingevulde informatie:\n");
+    for (int n = 0; n != i; ++n) {
+      printf("Naam: %s\tLeeftijd: %d\n", (pointer + n)->naam, (pointer + n)->leeftijd);
+    }
+    
+    pointer = (struct persoon *)realloc(pointer, sizeof(struct persoon) + sizeof(struct persoon));
+  }
 }
