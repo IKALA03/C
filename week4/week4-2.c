3465d11b077eb9526663f85b6e17182cf9a9d76d
IKALA03 <108133197+IKALA03@users.noreply.github.com>
2023-01-15 22:02:13 +0100

    Update week4-2.c

--- week4/week4-2.c
+++ week4/week4-2.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//
+
+struct person{
+    char name[20];
+    int age;
+};
 
 int main() {
-    struct persoon {
-    char naam[15];
-    int leeftijd;
-  };
-
-  struct persoon *pointer;
-  
-  pointer = (struct persoon *)malloc(sizeof(struct persoon));
-
-
-  int i = 0;
-  while(1) {
-    printf("Vul de naam en leeftijd in:\n");
-    scanf("%s %d", (pointer + i)->naam, &(pointer + i)->leeftijd);
-
-    i++;
-
-    printf("Ingevulde informatie:\n");
-    for (int n = 0; n != i; ++n) {
-      printf("Naam: %s\tLeeftijd: %d\n", (pointer + n)->naam, (pointer + n)->leeftijd);
+    struct person *p;
+    int n = 0;
+    int a = 1;
+    p=malloc(a*sizeof(struct person));
+    while (1)
+    {
+        printf("Naam: ");
+        scanf("%s", p[n].name);
+        printf("Leeftijd: ");
+        scanf("%d", &p[n].age);
+        n++;
+        a++;
+        p=realloc(p,a*sizeof(struct person));
+        
+        for (int j=0; j<n; j++) {
+            printf("Naam: %s, Leeftijd: %d", p[j].name, p[j].age);
+            printf("\n");
+        }
     }
-    
-    pointer = (struct persoon *)realloc(pointer, sizeof(struct persoon) + sizeof(struct persoon));
-  }
+    return 0;
 }
